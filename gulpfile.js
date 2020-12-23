ce20e7a5f67862b9cf50c423f31ea85702ce1457
psicodelico <guosj921@163.com>
2020-12-23 10:36:05 +0800

    js/css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
-const { series } = require('gulp');
+const { series, parallel, src, dest } = require('gulp');
 
 function clean(cb) {
     console.log('clean');
     cb();
 }
 
-function minify(cb) {
-    console.log('minify');
+function javascript(cb) {
+    console.log('javascript');
+    return src('app/src/*.js').pipe(dest('app/dist/'));
+}
+
+function css(cb) {
+    console.log('css');
     cb();
 }
 
 function transpile(cb) {
     console.log('transpile');
+    parallel(javascript, css)();
     cb();
 }
 
@@ -20,9 +26,14 @@ function livereload(cb) {
     cb();
 }
 
+function minify(cb) {
+    console.log('minify');
+    cb();
+}
+
 function build(cb) {
     console.log('build');
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'pro') {
         series(transpile, minify)();
     } else {
         series(transpile, livereload)();
