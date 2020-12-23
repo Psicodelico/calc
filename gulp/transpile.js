12a0cf8fff56fd86b34553354680cbb0c70e266e
psicodelico <guosj921@163.com>
2020-12-23 17:58:54 +0800

    add babel

--- gulp/transpile.js
+++ gulp/transpile.js
@@ -1,17 +1,14 @@
 const { parallel, src, dest } = require('gulp');
 
 function javascript(cb) {
-    console.log('javascript');
     return src('app/src/*.js').pipe(dest('app/dist/'));
 }
 
 function css(cb) {
-    console.log('css');
     cb();
 }
 
 function transpile(cb) {
-    console.log('transpile');
     parallel(javascript, css)();
     cb();
 }
--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,18 @@ const { series, parallel } = require('gulp');
 const transpile = require('./gulp/transpile');
 
 function clean(cb) {
-    console.log('clean');
     cb();
 }
 
 function livereload(cb) {
-    console.log('livereload');
     cb();
 }
 
 function minify(cb) {
-    console.log('minify');
     cb();
 }
 
 function build(cb) {
-    console.log('build');
     if (process.env.NODE_ENV === 'pro') {
         series(transpile, minify)();
     } else {
