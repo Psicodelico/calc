31d14c80e77e0913f5b8225271089de156fc5805
psicodelico <guosj921@163.com>
2020-12-24 17:22:52 +0800

    generator supported

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,12 @@
-let a = 1;
+function* hello() {
+    yield 1;
+    yield 2;
+    return 0;
+}
+
+let h = hello();
+
+console.log(h.next());
+console.log(h.next());
+console.log(h.next());
+console.log(h.next());
