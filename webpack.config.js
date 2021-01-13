b325e3977182d86baba71ed11e38eebabad20980
psicodelico <guosj921@163.com>
2021-01-13 10:43:53 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './app/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'app', 'dist'),
+        filename: 'index.js'
+    }
+}
